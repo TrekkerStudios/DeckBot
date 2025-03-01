e05925b62f2b5fc2a152d77952622a3195a9007c
Tyler B. <TrekkerStudios@users.noreply.github.com>
2025-03-01 02:45:46 +0000

    2h47m why wont cmds register

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import { config } from "./config";
 import { commands } from "./commands";
 import { deployCommands } from "./deploy-commands";
-import type { Gamestate } from "./types";
 
-const client = new Client({
-  intents: ["Guilds", "GuildMessages", "DirectMessages"],
+export const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
 });
 
 client.once("ready", () => {
-  console.log("DeckBot is ready!");
+  console.log("Discord bot is ready! 🤖");
 });
 
-client.on("guildCreate", async (guild) => {
-  await deployCommands({ guildId: guild.id });
+client.on("connect", async () => {
+  await deployCommands({ guildId: config.DISCORD_GUILD_ID });
 });
 
 client.on("interactionCreate", async (interaction) => {
